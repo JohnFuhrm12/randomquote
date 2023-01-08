c917e68a1e283013e0c8aad53f1facd62780c230
JohnFuhrm12 <61069716+JohnFuhrm12@users.noreply.github.com>
2023-01-08 17:24:13 -0300

    CSS Changes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import './App.css';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
+import bird from './bird.png';
+
 function App() {
   const [quote, setQuote] = useState('');
   const [author, setAuthor] = useState('');
 
   let randomNum = Math.floor(Math.random() * 10);
+  let tweetPath = `https://twitter.com/intent/tweet?text=%22If%20you%20do%20what%20you%E2%80%99ve%20always%20done%2C%20you%E2%80%99ll%20get%20what%20you%E2%80%99ve%20always%20gotten.%22%20Tony%20Robbins`
 
   useEffect(() => {
     getQuote();
@@ -32,7 +35,7 @@ function App() {
         <h1 id="text">"{quote}"</h1>
         <h2 id="author">- {author}</h2>
         <div id="bottom-row">
-          <a id="tweet-quote" href="twitter.com/intent/tweet">Tweet</a>
+            <a id="tweet-quote" target="blank" href={tweetPath}><img id="bird" src={bird}></img></a>
           <button id="new-quote" onClick={getQuote}>New Quote</button>
         </div>
       </div>
