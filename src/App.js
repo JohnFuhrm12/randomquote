203263c04b78b67906e043baa39418166dbf1379
JohnFuhrm12 <61069716+JohnFuhrm12@users.noreply.github.com>
2023-01-08 16:59:52 -0300

    Axios API call

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+
+  let randomNum = Math.floor(Math.random() * 10);
+
+  useEffect(() => {
+    getQuote();
+  }, []);
+
+  const getQuote = async (e) => {
+    randomNum = Math.floor(Math.random() * 10);
+
+    axios.get('https://type.fit/api/quotes').then(function(res) {
+      setQuote(res.data[randomNum].text);
+      setAuthor(res.data[randomNum].author);
+
+      //console.log(res.data[0].author);
+      //console.log(res.data[0].text);
+    });
+  }
+
+
   return (
     <>
-      <h1>Random Quote</h1>
+    <div id="page">
+      <div id="quote-box">
+        <h1 id="text">"{quote}"</h1>
+        <h2 id="author">- {author}</h2>
+        <div id="bottom-row">
+          <a id="tweet-quote" href="twitter.com/intent/tweet">Tweet</a>
+          <button id="new-quote" onClick={getQuote}>New Quote</button>
+        </div>
+      </div>
+    </div>
     </>
   );
 }
